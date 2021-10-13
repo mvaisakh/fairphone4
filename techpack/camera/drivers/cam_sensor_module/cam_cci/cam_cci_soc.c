7fb1f302cbb11618cb8a829eef1c8e06b735996a
Junwen YE <junwen.ye@jrdcom.com>
2021-10-13 17:32:38 +0800

    Revert "[ALM:11087279] [FP4]:catch ois data after cci is ready."
    This reverts commit ea589bfbd59bacaff438882b1890ec1d934abf7e.
    
    Change-Id: I0a811dc74e0cdfc2ad4d8be18e7c269f8a4ff675

--- techpack/camera/drivers/cam_sensor_module/cam_cci/cam_cci_soc.c
+++ techpack/camera/drivers/cam_sensor_module/cam_cci/cam_cci_soc.c
@@ -6,8 +6,6 @@
 #include "cam_cci_dev.h"
 #include "cam_cci_core.h"
 
-enum cam_cci_state_t cci_state_for_vsync = 0;
-
 int cam_cci_init(struct v4l2_subdev *sd,
 	struct cam_cci_ctrl *c_ctrl)
 {
@@ -189,7 +187,6 @@ int cam_cci_init(struct v4l2_subdev *sd,
 	}
 
 	cci_dev->cci_state = CCI_STATE_ENABLED;
-	cci_state_for_vsync = 1;
 
 	return 0;
 
@@ -426,7 +423,6 @@ int cam_cci_soc_release(struct cci_device *cci_dev)
 	}
 
 	cci_dev->cci_state = CCI_STATE_DISABLED;
-	cci_state_for_vsync = 0;
 	cci_dev->cycles_per_us = 0;
 
 	cam_cpas_stop(cci_dev->cpas_handle);
